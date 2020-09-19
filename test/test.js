707335e1c09e8729a324852eabea37d88d380f40
Snowflake107 <devsnowflake107@gmail.com>
2020-09-19 18:21:47 +0545

    test

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@ const db = new Database("mongodb://localhost/test");
 
 db.on("ready", () => {
     console.log("Hey, im connected!");
+    db.set("foo", "bar").then(() => db.export("./database.json"))
 });
 
 db.on("error", console.error);
