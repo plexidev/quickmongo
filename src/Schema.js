e41c4e6e11caf60d088c07433e7d7649d2cc876f
Snowflake107 <devsnowflake107@gmail.com>
2020-09-27 01:30:37 +0545

    fix schema error

--- src/Schema.js
+++ src/Schema.js
@@ -1,16 +1,16 @@
 const { Schema, model } = require("mongoose");
 
-module.exports = (name) => {
-    const Default = new Schema({
-        ID: {
-            type: Schema.Types.String,
-            required: true
-        },
-        data: {
-            type: Schema.Types.Mixed,
-            required: true
-        }
-    });
+const Default = new Schema({
+    ID: {
+        type: Schema.Types.String,
+        required: true
+    },
+    data: {
+        type: Schema.Types.Mixed,
+        required: true
+    }
+});
 
+module.exports = (name) => {
     return typeof name === "string" ? model(name, Default) : model("JSON", Default);
 };
