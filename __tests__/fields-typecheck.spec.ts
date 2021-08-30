75772ec21d18b9b7ee9181a6f0e37a52ec6ddef7
DevAndromeda <46562212+DevAndromeda@users.noreply.github.com>
2021-08-30 11:14:18 +0545

    test(Fields): add unit tests

---
+++ __tests__/fields-typecheck.spec.ts
@@ -0,0 +1,64 @@
+import { AnyField, ArrayField, BooleanField, NullableField, NumberField, ObjectField, StringField } from "../src/fields";
+
+describe("Fields Type Check", () => {
+    test("AnyField", () => {
+        const any = new AnyField();
+
+        expect(any.create(undefined)).toBe(undefined);
+    });
+
+    test("ArrayField", () => {
+        const array = new ArrayField(new StringField());
+
+        expect(array.create(["a"])).toBe(["a"]);
+
+        // @ts-expect-error Must be an array of strings
+        expect(array.create([1])).toThrowError();
+
+        // @ts-expect-error Must be an array of strings
+        expect(array.create(1)).toThrowError();
+    });
+});
+
+// const boolean = new BooleanField();
+
+// boolean.create(true);
+
+// // @ts-expect-error Must be a boolean
+// boolean.create(1);
+
+// const nullable = new NullableField(new StringField());
+
+// nullable.create(null);
+
+// // @ts-expect-error Must be a string or null
+// nullable.create(2);
+
+// const number = new NumberField();
+
+// number.create(1);
+
+// // @ts-expect-error Must be a number
+// number.create(false);
+
+// const object = new ObjectField({
+//     a: new BooleanField(),
+//     b: new NumberField()
+// });
+
+// object.create({
+//     a: true,
+//     b: 1
+// });
+
+// object.create({
+//     // @ts-expect-error Key does not exist
+//     c: "foo"
+// });
+
+// const string = new StringField();
+
+// string.create("a");
+
+// // @ts-expect-error Must be a string
+// string.create(1);
