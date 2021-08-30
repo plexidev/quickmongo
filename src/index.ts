b95cdb2f7f662e1984ead560b53468278c0cffcc
ZYROUGE <zyrouge@hotmail.com>
2021-08-30 11:37:30 +0530

    refactor: normalize filename

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export { Collection } from "./Collection";
-export * as Fields from "./fields/index";
+export { Collection } from "./collection";
+export * as Fields from "./fields";
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 export const version = require("../package.json").version;
