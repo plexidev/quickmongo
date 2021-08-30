9e7488b7f87520049519982a5e19726f9df23418
DevAndromeda <46562212+DevAndromeda@users.noreply.github.com>
2021-08-30 11:56:07 +0545

    test(fields): update imports

--- __tests__/fields.spec.ts
+++ __tests__/fields.spec.ts
@@ -1,45 +1,45 @@
-import { AnyField, ArrayField, BooleanField, FieldModel, NullableField, NumberField, ObjectField, StringField } from "../src/fields";
+import { Fields } from "../src/index";
 
 describe("test fields", () => {
     test("model field", () => {
-        const field = new FieldModel();
+        const field = new Fields.FieldModel();
         expect(field.create.bind(field)).toThrowError();
     });
 
     test("any field", () => {
-        const any = new AnyField();
+        const any = new Fields.AnyField();
         expect(any.create(true)).toBe(true);
     });
 
     test("array field", () => {
-        const array = new ArrayField(new StringField());
+        const array = new Fields.ArrayField(new Fields.StringField());
         expect(array.create(["a"])).toStrictEqual(["a"]);
         expect(array.create.bind(array, [1])).toThrow(TypeError);
         expect(array.create.bind(array, 1)).toThrow(TypeError);
     });
 
     test("boolean field", () => {
-        const boolean = new BooleanField();
+        const boolean = new Fields.BooleanField();
         expect(boolean.create(true)).toBe(true);
         expect(boolean.create.bind(boolean, 1)).toThrow(TypeError);
     });
 
     test("nullable field", () => {
-        const nullable = new NullableField(new StringField());
+        const nullable = new Fields.NullableField(new Fields.StringField());
         expect(nullable.create(null)).toBe(null);
         expect(nullable.create.bind(nullable, 2)).toThrow(TypeError);
     });
 
     test("number field", () => {
-        const number = new NumberField();
+        const number = new Fields.NumberField();
         expect(number.create(1)).toBe(1);
         expect(number.create.bind(number, false)).toThrow(TypeError);
     });
 
     test("object field", () => {
-        const object = new ObjectField({
-            a: new BooleanField(),
-            b: new NumberField()
+        const object = new Fields.ObjectField({
+            a: new Fields.BooleanField(),
+            b: new Fields.NumberField()
         });
     
         expect(object.create({
@@ -56,7 +56,7 @@ describe("test fields", () => {
     });
 
     test("string field", () => {
-        const string = new StringField();
+        const string = new Fields.StringField();
         expect(string.create("a")).toBe("a");
         expect(string.create.bind(string, 1)).toThrow(TypeError);
     });
